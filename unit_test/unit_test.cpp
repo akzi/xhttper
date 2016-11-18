132a0f53b98063aa114521a52ad772c5805dce50
akzi <382018309@qq.com>
2016-11-18 13:46:12 +0100

    add codes

--- unit_test/unit_test.cpp
+++ unit_test/unit_test.cpp
@@ -43,29 +43,31 @@ XTEST_SUITE(xhttper)
 		per.reset();
 		xassert(per.get_string(strlen("hello world"))== "hello world");
 	}
-#if 0
+#if 2
 	XUNIT_TEST(Benchmark)
 	{
 		const char *buf = "GET /cookies HTTP/1.1\r\nHost: 127.0.0.1:8090\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\nCookie: name=wookie\r\n\r\n";
+		auto buflen =  strlen(buf);
 		int64_t count = 0;
 		std::thread counter([&] {
 			auto last_count = count;
+			auto count_ = count;
 			auto len = strlen(buf);
 			do
 			{
-				std::cout << 1.0 / ((count - last_count) / 100000.0) << std::endl;
-				last_count = count;
+				count_ = count;
+				std::cout << (count_ - last_count)*buflen / 1024 / 1024 << " MB " << (count_ - last_count) << std::endl;
+				last_count = count_;
 				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			} while (1);
 		});
 
 		xhttper::parser per;
-		per.append(buf, strlen(buf));
 		do
 		{
-			xassert(per.parse_req());
-			per.reset_status();
 			per.append(buf, strlen(buf));
+			xassert(per.parse_req());
+			per.reset();
 			count++;
 		} while (1);
 	}
